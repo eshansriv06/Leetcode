adcc9f8fe34ad437a1e30a2931e4d462b3473661
Eshan Srivastava <eshan.sriv@gmail.com>
2026-08-10 15:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (13.94%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int x:nums)
+          mp[x]++;
+        int c=0;
+          for(auto x:mp)
+          {
+            if(x.second>floor(n/2))
+            c=x.first;
+          }
+          return c;
+    }
+};
